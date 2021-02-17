362bb4cc28257d83f30054d63d4d062acb5c4f88
Michael Conrads <michaelconrads@me.com>
2021-02-17 22:29:47 +0100

    Update keyboard init script

--- usq/msc/setup_keyboard.sh
+++ usq/msc/setup_keyboard.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 
-# set key repeat rate. delay 280 milisecond, 40 per sec
-xset r rate 200 40
+# The first  specifies the  delay  before autorepeat starts and the second specifies the
+# repeat rate. The  delay is the number of milliseconds before autorepeat starts, and the
+# rate is the number of repeats per second.
+
+xset r rate 200 35
 
